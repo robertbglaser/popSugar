53850578864f8a8e9bb6aaf8f072254d453be26a
Robert Glaser <robertbglaser@gmail.com>
2019-02-12 14:55:13 -0800

    initial commit"

---
+++ script.js
@@ -0,0 +1,26 @@
+
+
+
+function runSimulation(){
+
+    console.log("a click event has been heard");
+    var chairs =  document.getElementById("chairCount").value;
+    var milliSeconds = document.getElementById("delay").value;
+    console.log(chairs, milliSeconds);
+}
+
+function singleStep (){
+    console.log("a single step click event has been heard");
+    var chairs =  document.getElementById("chairCount").value;
+    var milliSeconds = document.getElementById("delay").value;
+    console.log(chairs, milliSeconds);
+
+} 
+
+function reset (){
+
+    console.log("a reset click event has been heard");
+    var chairs =  document.getElementById("chairCount").value;
+    var milliSeconds = document.getElementById("delay").value;
+    console.log(chairs, milliSeconds);
+}
