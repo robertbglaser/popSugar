500e99513e876c49ae663c5f7ad9a9d26abd5e0f
Robert Glaser <robertbglaser@gmail.com>
2019-02-12 17:09:33 -0800

    added the setInterval and clearInterval functions.  tested

--- script.js
+++ script.js
@@ -4,16 +4,22 @@
 function runSimulation(){
 
     console.log("a click event has been heard");
-    var chairs =  document.getElementById("chairCount").value;
+    var chairLimit =  document.getElementById("chairCount").value;
     var milliSeconds = document.getElementById("delay").value;
-    console.log(chairs, milliSeconds);
+    console.log(chairLimit, milliSeconds);
+     let timerId = setInterval(() => console.log('tick'), milliSeconds);
+
+     setTimeout(() => { clearInterval(timerId); console.log('stop'); }, 5000);
 }
 
 function singleStep (){
-    console.log("a single step click event has been heard");
     var chairs =  document.getElementById("chairCount").value;
     var milliSeconds = document.getElementById("delay").value;
     console.log(chairs, milliSeconds);
+
+    for (var charNum = 0; chairsRemaining = 1; charNum ++){
+
+    }
 
 } 
 
